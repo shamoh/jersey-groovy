f9edc45dc022d1094bac8769f550e50730a32b18
Libor Kramolis <libor.kramolis@oracle.com>
2013-06-08 01:09:06 +0200

    Template prepared.

--- slides/slide_config.js
+++ slides/slide_config.js
@@ -1,15 +1,15 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
-    subtitle: 'Subtitle Goes Here',
-    //eventTitle: 'Google I/O 2013',
+    title: 'JAX-RS Client API<br>Jersey Groovy DSL',
+    subtitle: 'Custom REST DSL prototype',
+    eventTitle: 'Sabbatical Week',
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
     //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/google_developers_logo_tiny.png',
+    favIcon: 'images/jersey_logo_tiny.png',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
       'Source Code Pro'
@@ -19,12 +19,12 @@ var SLIDE_CONFIG = {
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title, Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
+    name: 'Libor Kramolis',
+    company: 'Software Engeneer, Oracle',
+    gplus: 'https://plus.google.com/115270016494231681069',
+    twitter: '@liborkramolis',
+    www: 'http://kramolis.cz',
+    github: 'https://github.com/shamoh'
   }/*, {
     name: 'Second Name',
     company: 'Job Title, Google',
